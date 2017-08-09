58b5aab706b9a8b8c06abebf81854857962b0414
Barvazur <benamiamit0@gmail.com>
2017-08-09 23:43:59 +0300

    Added an endpoint

--- lambdaHandler.js
+++ lambdaHandler.js
@@ -1,19 +1,19 @@
-var compareFlow = require('./compare-flow');
+var compareFlow = require('./compare-flow'),
+    awsSns = require('./aws-sns');
+
+// Notifying
+var notificationHandler = function (event, context, callback) {
+    console.info('Sending a notification to the subscribers');
+    awsSns.sendSMS();
+};
 
 // When new face is required for learning
-exports.subscribeNewFace = function (event, context, callback) {
+exports.learnNewFace = function (event, context, callback) {
     console.info('Subscribing new face');
-
-    // compareFlow.addFaceToCollection();
+    compareFlow.addFaceToCollection(event.Records[0].s3.object.key);
 };
 
 // Comparing between faces
 exports.faceComparer = function (event, context, callback) {
     console.info('Starting the face comparison');
-    console.info(event.Records[0].s3.bucket.name);
-    console.info(event.Records[0].s3.object.key);
-};
-
-// Notifying
-exports.notificationHandler = function (event, context, callback) {
 };
