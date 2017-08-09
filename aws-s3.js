ca7370433f226f377713193a9436c5a1428e4623
Barvazur <benamiamit0@gmail.com>
2017-08-09 17:09:03 +0300

    Added a service for getting all images in bucket

--- aws-s3.js
+++ aws-s3.js
@@ -1,30 +1,54 @@
 var aws = require('aws-sdk');
 var imageHandler = require('./image-handler.js');
-
 var s3 = new aws.S3();
-var bucketName = 'ChenKu';
 
-var uploadImagesToBucket = function () {
+module.exports = {
+    bucketName: 'ChenKu',
+    uploadtoBucket: function () {
+        var uploadImagesToBucket = function () {
 
-    var specificImageCallback = function (name, extension, loadedImageData) {
-        var params = {
-            Bucket: bucketName,
-            Key: name + '.' + extension,
-            Body: loadedImageData,
-            Tagging: 'name=' + name
+            var specificImageCallback = function (name, extension, loadedImageData) {
+                var params = {
+                    Bucket: bucketName,
+                    Key: name + '.' + extension,
+                    Body: loadedImageData,
+                    Tagging: 'name=' + name
+                };
+
+                s3.putObject(params, function (err, data) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    else {
+                        console.info('Successfully uploaded an image of ' + name + ' to the bucket ' + bucketName);
+                    }
+                });
+            }
+
+            imageHandler.readImages(specificImageCallback);
         };
 
-        s3.putObject(params, function (err, data) {
-            if (err) {
-                console.error(err);
+        s3.createBucket({ Bucket: bucketName }, uploadImagesToBucket);
+    },
+    getAllImagesFromBucket: function (callback, marker) {
+
+        var objectListBucketCallback = function (err, data) {
+            if (err) throw err;
+
+            if (callback && typeof(callback) == 'function') {
+                if (data.IsTruncated) {
+                    getAllImagesFromBucket(callback, data.NextMarker)
+                }
+                else {
+                    var images = [];
+                    for (var imageIndex = 0; imageIndex < data.Contents.length; imageIndex++) {
+                        images.push(data.Contents[imageIndex].Key);
+                    }
+                    callback(images);
+                }
             }
-            else {
-                console.info('Successfully uploaded an image of ' + name + ' to the bucket ' + bucketName);
-            }
-        });
+        };
+
+        s3.listObjects({Bucket: bucketName, Marker: marker}, objectListBucketCallback);
     }
-
-    imageHandler.readImages(specificImageCallback);
 };
-
-s3.createBucket({Bucket: bucketName}, uploadImagesToBucket);
